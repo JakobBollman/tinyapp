7d66215b0eff864f0076bca2685d2bf09124a6b9
JakobBollman <jakob@bollman.ca>
2022-10-04 15:41:13 -0600

    yeet

--- express_server.js
+++ express_server.js
@@ -3,6 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+const generateRandomString = function() {
+  let newID = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( var i = 0; i < 6; i++ ) {
+    newID += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return newID;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,13 +44,28 @@ app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 
+app.post("/urls", (req, res) => {
+  let newId = generateRandomString();
+  urlDatabase[newId] = req.body["longURL"];
+  res.redirect(`/urls/${newId}`);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:id", (req, res) => {
+  longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
