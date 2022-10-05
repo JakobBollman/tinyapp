ef270a0825bd85f5b63cf9acd299ca506df3534d
JakobBollman <jakob@bollman.ca>
2022-10-05 15:07:42 -0600

    cookie

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 const generateRandomString = function() {
   let newID = '';
@@ -50,28 +52,44 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newId}`);
 });
 
+app.post("/login", (req, res) => {
+
+  res.cookie("username",req.body["userName"]);
+  const templateVars = {username: req.cookies["username"], urls: urlDatabase};
+  console.log(req.cookies["username"]);
+  //res.render("urls_index", templateVars);
+  res.redirect("/urls");
+
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"], urls: urlDatabase };
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   delete urlDatabase[templateVars.id];
   res.redirect('/urls')
 });
 
 app.post("/urls/:id/edit", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   urlDatabase[templateVars.id] = req.body["newLong"];
   res.redirect(`/urls/${templateVars.id}`);
 });
