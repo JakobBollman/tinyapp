128cd6c6cfb34e1b44684a97529e307fa58e01e1
JakobBollman <jakob@bollman.ca>
2022-10-06 10:37:06 -0600

    yee

--- express_server.js
+++ express_server.js
@@ -21,6 +21,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  '4bTa9': {
+    id: "4bTa9",
+    email: "user1@example.com",
+    password: "Chop-Suey",
+  },
+  'T5h2a': {
+    id: "T5h2a",
+    email: "user2@example.com",
+    password: "Brass-Monkey",
+  },
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -52,44 +65,90 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newId}`);
 });
 
-app.post("/login", (req, res) => {
-
-  res.cookie("username",req.body["userName"]);
-  const templateVars = {username: req.cookies["username"], urls: urlDatabase};
-  console.log(req.cookies["username"]);
-  //res.render("urls_index", templateVars);
-  res.redirect("/urls");
-
-});
+// app.post("/login", (req, res) => {
+//   res.cookie("username",req.body["userName"]);
+//   const templateVars = {username: req.cookies["username"], urls: urlDatabase};
+//   res.redirect("/urls");
+// });
 
 app.post("/logout", (req, res) => {
   res.clearCookie('username');
+  res.clearCookie('userid');
   res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {username: req.cookies["username"], urls: urlDatabase };
+  const templateVars = {username: req.cookies["username"], userid: req.cookies["userid"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/login", (req, res) => {
+  const templateVars = {username: req.cookies["username"], userid: req.cookies["userid"], urls: urlDatabase };
+  res.render("urls_login", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  for(let item in users){
+    if(req.body["email"] === users[item]['email']){
+      if(req.body['password'] === users[item]['password']){
+        res.cookie("userid",users[item]['id']);
+        res.cookie("username",users[item]);
+        res.redirect("/urls");
+      } else {
+        res.sendStatus(403);
+      }
+    }
+  }
+  res.sendStatus(403);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = {username: req.cookies["username"], userid: req.cookies["userid"], urls: urlDatabase };
+  res.render("urls_register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  let copy = false;
+  for(let item in users){
+    if(req.body["email"] === users[item]['email']){
+      copy = true;
+    }
+  }
+  if((req.body["email"].length < 1) || (req.body["password"].length < 1)){
+    res.sendStatus(400);
+  } else if(copy){
+    res.sendStatus(400);
+  } else {
+  let genId = generateRandomString();
+  users[genId] = {};
+  users[genId]['id'] = genId;
+  
+  users[genId]['email'] = req.body["email"];
+  users[genId]['password'] = req.body["password"];
+  res.cookie("userid",users[genId]['id']);
+  res.cookie("username",users[genId]);
+  res.redirect("/urls");
+  }
+});
+
 app.get("/urls/new", (req, res) => {
-  const templateVars = {username: req.cookies["username"], urls: urlDatabase };
+  const templateVars = {username: req.cookies["username"], userid: req.cookies["userid"], urls: urlDatabase };
   res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {username: req.cookies["username"], userid: req.cookies["userid"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const templateVars = {username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {username: req.cookies["username"], userid: req.cookies["userid"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   delete urlDatabase[templateVars.id];
   res.redirect('/urls')
 });
 
 app.post("/urls/:id/edit", (req, res) => {
-  const templateVars = {username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {username: req.cookies["username"], userid: req.cookies["userid"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   urlDatabase[templateVars.id] = req.body["newLong"];
   res.redirect(`/urls/${templateVars.id}`);
 });
